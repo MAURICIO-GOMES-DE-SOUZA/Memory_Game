0b2029fd46084e4192533f8ebc138dc93e8ed0de
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2023-11-29 19:28:13 -0300

    add button navigate to rank

--- scripts/newGame.js
+++ scripts/newGame.js
@@ -1,8 +1,3 @@
-const inputName = document.querySelector(".inputName");
-const newGameButton = document.querySelector(".newGameButton");
-const newGameForm = document.querySelector(".newGameForm");
-
-
 function validateInput(event) { 
     if (event.target.value.length >= 3) {
         newGameButton.removeAttribute("disabled")
@@ -20,8 +15,21 @@ function handleSubmintNewGame(event) {
     window.location.href = "pages/cards.html";
 
 }
+function navigateToRank() {
+    window.location.href = "pages/rank.html";
+}
+
+const inputName = document.querySelector(".inputName");
+const newGameButton = document.querySelector(".newGameButton");
+const newGameForm = document.querySelector(".newGameForm");
+const buttonRank = document.querySelector(".buttonRank");
+
+
+
+
 
 inputName.addEventListener("input", validateInput);
 newGameForm.addEventListener("submit", handleSubmintNewGame);
+buttonRank.addEventListener("click", navigateToRank);
 
 localStorage.removeItem("myKeyAplication")
