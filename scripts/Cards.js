53576e1d7a64b6ca556e0b2ad1fdef63ab3ab43d
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2023-11-28 21:30:36 -0300

    add functions setStartTimer and checkGameWin

--- scripts/Cards.js
+++ scripts/Cards.js
@@ -36,7 +36,6 @@ function createCards() {
     () => Math.random() - 0.5
   );
 
-
   gridCards.innerHTML = "";
   sortedCards.forEach((card) => {
     gridCards.innerHTML += `
@@ -52,25 +51,53 @@ function createCards() {
   });
 }
 
-function checkMatchCards() {
-    if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
-        new Audio("../audios/sci-fi.wav").play();
-        setTimeout(() => {
-            firstCard.classList.add("disabledCard")
-            secondCard.classList.add("disabledCard");
-            firstCard = "";
-            secondCard = "";
-            
-        }, 400)
-    } else {
-        setTimeout(() => {
-            firstCard.classList.remove("flipCard")
-            secondCard.classList.remove("flipCard")
-            firstCard = "";
-            secondCard = "";
-            }, 300)
+function checkGameWin() {
+  const disabledCards = document.querySelectorAll(".disabledCard");
 
-        }
+  if (disabledCards.length === 2) {
+    clearInterval(finishTimerInterval);
+
+    const userDate = {
+      name: storagePlayerName,
+      timer: timer.textContent,
+    };
+
+    const storageRank = JSON.parse(localStorage.getItem("@memoryGame:rank"))
+    if(storageRank) {
+      console.log(storageRank);
+      const rankData = [...storageRank, userDate]
+      localStorage.setItem("@memoryGame:rank", JSON.stringify( rankData));
+
+    } else {
+
+      localStorage.setItem("@memoryGame:rank", JSON.stringify([userDate]));
+    }
+
+    alert(
+      `Parabéns ${storagePlayerName}, você venceu  com o tempo de ${timer.innerHTML}!`
+    );
+  }
+}
+
+function checkMatchCards() {
+  if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
+    new Audio("../audios/sci-fi.wav").play();
+    setTimeout(() => {
+      firstCard.classList.add("disabledCard");
+      secondCard.classList.add("disabledCard");
+      firstCard = "";
+      secondCard = "";
+
+      checkGameWin();
+    }, 400);
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("flipCard");
+      secondCard.classList.remove("flipCard");
+      firstCard = "";
+      secondCard = "";
+    }, 300);
+  }
 }
 
 function clickFlipCard() {
@@ -85,20 +112,29 @@ function clickFlipCard() {
         card.classList.add("flipCard");
         firstCard = card;
       } else if (secondCard === "") {
-        card.classList.add("flipCard")
+        card.classList.add("flipCard");
         secondCard = card;
         checkMatchCards();
-
       }
-
-
     });
   });
+}
+
+function setStartTime() {
+  finishTimerInterval = setInterval(() => {
+    const dateNow = new Date();
+    const dateDiff = new Date(dateNow - initialDateTimer);
+    const minutes = String(dateDiff.getMinutes()).padStart("2", "0");
+    const seconds = String(dateDiff.getSeconds()).padStart("2", "0");
+
+    timer.innerHTML = `${minutes}:${seconds}`;
+  }, 1000);
 }
 
 const playerName = document.querySelector(".playerName");
 const backButton = document.querySelector(".backButton");
 const gridCards = document.querySelector(".gridCards");
+const timer = document.querySelector(".timer");
 
 const storagePlayerName = localStorage.getItem("@memoryGame:playerName");
 
@@ -111,3 +147,7 @@ createCards();
 let firstCard = "";
 let secondCard = "";
 clickFlipCard();
+
+const initialDateTimer = new Date();
+let finishTimerInterval;
+setStartTime();
